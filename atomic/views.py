90f87caafc3e5bbfa8e5efe5a3e809ca48e656ae
Carlos Leon <35080984+chinchay@users.noreply.github.com>
2023-06-02 16:24:41 -0300

    feat: Render about.html

--- atomic/views.py
+++ atomic/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world. You are at the app index, app.views.index")
+    # return HttpResponse("Hello world. You are at the app index, app.views.index")
+    return render(request, "base.html")
 
 def tracker(request):
     return HttpResponse("Hello from app.views.tracker")
 
 def about(request):
-    text  = "The purpose of this Habit Tracker Web application is to help you to maintain good habits to reach your goals through gamification (turning your goals into a game)."
-    return HttpResponse(text)
+    text  = "This web app will help you with maintaining good habits through gamification (turning goals into games)."
+    # return HttpResponse(text)
+    return render(request, "atomic/about.html", {"text":text})
 
