5e6464b9a7bb2f3b86c44ba872b00fcb4ef30ad7
Carlos Leon <35080984+chinchay@users.noreply.github.com>
2023-06-02 17:00:52 -0300

    feat: Add Django dynamic linking

--- atomic/urls.py
+++ atomic/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+################################################################################
+# Registering the app namespace...
+# this will allow you to create dynamic Django hyperlinks in html files
+# when using the django tag: {% url atomic:tracker ... %} for example.
+app_name = "atomic"
+################################################################################
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("tracker/", views.tracker, name="tracker"),
